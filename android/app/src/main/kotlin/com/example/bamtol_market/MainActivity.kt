da75575b3aa59c7c3a1475be5082aa0db90efbbd
938938 <938kjh@gmail.com>
2025-11-21 23:00:34 +0900

    [FEAT] 프로젝트 준비

---
+++ android/app/src/main/kotlin/com/example/bamtol_market/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.bamtol_market
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Image.asset(
+              'assets/images/logo_simbol.png',width: 150,
+                              height: 150,
+          ),
+        ),
+      ),
+    );
+  }
+}
