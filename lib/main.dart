c6ca4e8d1deee0ba98addc793dd196afde605753
938938 <938kjh@gmail.com>
2025-11-23 22:08:25 +0900

    [FEAt] GetX 라우트 설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'src/app.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         body: Center(
+//           child: Image.asset(
+//               'assets/images/logo_simbol.png',width: 150,
+//                               height: 150,
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Image.asset(
-              'assets/images/logo_simbol.png',width: 150,
-                              height: 150,
-          ),
-        ),
-      ),
+    return GetMaterialApp(
+      title: '당근마켓 클론 코딩',
+      initialRoute: '/',
+      getPages: [
+        GetPage(name: '/', page: ()=> const App()),
+      ],
     );
   }
 }
