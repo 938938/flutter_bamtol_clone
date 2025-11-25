fde55086bd94ce29dc9e51797298c902b1e25a03
938938 <938kjh@gmail.com>
2025-11-25 21:28:56 +0900

    [FEAT] 라이브러리 적용

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,14 +1,25 @@
+import 'package:bamtol_market/main.dart';
+import 'package:bamtol_market/src/init/page/init_start_page.dart';
+import 'package:bamtol_market/src/splash/page/splash_page.dart';
 import 'package:flutter/material.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
 
   @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  late bool isInitStarted;
+
+  @override
+  void initState() {
+    super.initState();
+    isInitStarted = prefs.getBool('isInitStarted') ?? true;
+  }
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Image.asset('assets/images/logo_simbol.png'),
-      ),
-    );
+    return isInitStarted ? const InitStartPage() : const SplashPage();
   }
 }
+
